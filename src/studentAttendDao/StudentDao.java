f90816fdfcf2da080f92d165e0888b99df63157a
shu-g <kksasasa1177@gmail.com>
2021-01-16 22:16:26 +0900

    Beanの追加

---
+++ src/studentAttendDao/StudentDao.java
@@ -0,0 +1,5 @@
+package studentAttendDao;
+
+public class StudentDao {
+
+}
--- src/studentAttendServlet/StudentAttend.java
+++ src/studentAttendServlet/StudentAttend.java
@@ -1,6 +1,9 @@
 package studentAttendServlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +19,11 @@ public class StudentAttend extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("message", "出欠情報を登録してください");
+
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher("/disp.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
